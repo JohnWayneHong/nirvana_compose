07bd2f3610040d89f213d2acf6f3f42b57586ba8
hwj <hwj@1972739551887735.onaliyun.com>
2026-04-11 17:34:21 +0800

    build(version): 版本发布v4.1.0,  code:3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.ggb.wanandroidcompose"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "4.0"
+        versionCode = 3
+        versionName = "4.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
